bd396c6dcb4cccc7848c20989a432fc959c31926
kaheiseki <kaheiseki.code@gmail.com>
2021-12-27 17:39:30 +0900

    add components

--- src/index.js
+++ src/index.js
@@ -23,6 +23,9 @@ const Root = () => {
         <div className = "gameinfo"><GameInfo/></div>
         <div className = "gameinfo"><GameInfo/></div>
         <div className = "gameinfo"><GameInfo/></div>
+        <div className = "gameinfo"><GameInfo/></div>
+        <div className = "gameinfo"><GameInfo/></div>
+        <div className = "gameinfo"><GameInfo/></div>
       </div>
     </div>
   )
