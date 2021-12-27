5bfbcbce06293f9f4452df7264d0347a13690781
kaheiseki <kaheiseki.code@gmail.com>
2021-12-27 21:40:47 +0900

    add firebase settings

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import {initializeApp} from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAn334ADTDl1gYSpQDyzf6dFGgpUlA6Xns",
+  authDomain: "sandlot-e5836.firebaseapp.com",
+  projectId: "sandlot-e5836",
+  storageBucket: "sandlot-e5836.appspot.com",
+  messagingSenderId: "947698565979",
+  appId: "1:947698565979:web:9e8097dd364c84f269907e",
+  measurementId: "G-XEKC41LCER"
+}
+
+const app = initializeApp(firebaseConfig);
