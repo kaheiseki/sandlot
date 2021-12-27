b674ce0eaf0ccdd8670c443ec76b1dbb27f93842
kaheiseki <kaheiseki.code@gmail.com>
2021-12-27 19:25:56 +0900

    move index

--- src/gameinfo.js
+++ src/gameinfo.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {Card,Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./gameinfo.css"
 
 const GameInfo = () => {
   let a = 1;
