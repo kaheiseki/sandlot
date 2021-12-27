26e5b10789be1bf2e88f41ca3736768f1d23308d
kyonishikawa <kyo24111@gmail.com>
2021-12-27 22:24:15 +0900

    modified file structure

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import {Link} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import Header from "./header/header"
-import GameInfo from "./index/gameinfo"
-import TeamInfo from "./teamTable/teaminfo"
+import Header from "./components/header/header"
+import GameInfo from "./pages/index/gameinfo"
+import TeamInfo from "./pages/teamTable/teaminfo"
 
 const Root = () => {
   return(
