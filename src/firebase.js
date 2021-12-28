48f14d0bcc17a4c79ae6ba091858c1a3a1dea6ff
kyonishikawa <kyo24111@gmail.com>
2021-12-28 19:42:08 +0900

    create a simple data add function

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
-import firebase from "firebase";
-
-const firebaseConfig = {
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+const firebaseApp = initializeApp({
   apiKey: "AIzaSyAn334ADTDl1gYSpQDyzf6dFGgpUlA6Xns",
   authDomain: "sandlot-e5836.firebaseapp.com",
   projectId: "sandlot-e5836",
@@ -8,9 +8,8 @@ const firebaseConfig = {
   messagingSenderId: "947698565979",
   appId: "1:947698565979:web:9e8097dd364c84f269907e",
   measurementId: "G-XEKC41LCER"
-}
+});
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestore();
+const db = getFirestore();
 
-export {db} ;
+export {db};
