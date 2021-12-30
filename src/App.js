cef9d05be040fe998819daff114460cf99c50114
kaheiseki <kaheiseki.code@gmail.com>
2021-12-30 17:58:13 +0900

    create add team components

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { CreateGamePost } from "./pages/create_game_post/create_game_post";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ErrorPage } from "./pages/error_page/error_page";
 import { MyTeam } from "./pages/my_team/my_team";
+import { CreateTeamPost } from "./pages/add_teams/add_team";
 
 
 const App = () => {
@@ -23,6 +24,7 @@ const App = () => {
             <Route path="/myteam" element={<MyTeam/>}/>
             <Route path="/creategame" element={<CreateGamePost/>}/>
             <Route path="/teamtable" element={<TeamInfo/>}/>
+            <Route path = "/createteam" element = {<CreateTeamPost/>}/>
             <Route path="*" element={<ErrorPage/>}/>
           </Routes>
         </BrowserRouter>
---
+++ src/pages/add_teams/add_team.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { useState, useEffect } from "react";
+import { db } from "../../firebase";
+
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card,Button} from 'react-bootstrap';
+
+export const CreateTeamPost = () => {
+
+  const [newTeamName, setNewTeamName] = useState("");
+  const [newCount, setNewCount] = useState(0);
+  const [newPlace, setNewPlace] = useState("");
+
+  const gamesCollectionRef = collection(db, "Teams");
+
+  const createTeam = async () => {
+    await addDoc(gamesCollectionRef, { name: newTeamName, place: newPlace, count: Number(newCount) });
+  };
+
+  return(
+    <div className='CreateGamePost'>
+      <Card style={{ width: "40rem" , height:"15rem",borderRadius:"10px"}}>
+        <Card.Body>
+          <Card.Title className='createGamePostTitle'>新規チーム作成</Card.Title>
+          <input
+            className='form-control'
+            placeholder="Team name..."
+            onChange={(event) => {
+              setNewTeamName(event.target.value);
+            }}
+          />
+          <input
+            className='form-control'
+            placeholder="Home Place..."
+            onChange={(event) => {
+              setNewPlace(event.target.value);
+            }}
+          />
+          <input
+            className='form-control'
+            type="number"
+            placeholder="Count..."
+            onChange={(event) => {
+              setNewCount(event.target.value);
+            }}
+          />
+        </Card.Body>
+        <form>
+          <Button variant="primary" style = {{width:"180px"}} onClick={createTeam}>
+              Create team
+          </Button>
+        </form>
+
+      </Card>
+
+
+    </div>
+
+  );
+}
