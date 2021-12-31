2866376db26bb799e81a4c6ea8929f7f8608c2d5
kyonishikawa <kyo24111@gmail.com>
2021-12-31 20:18:03 +0900

    edit post make

--- src/pages/create_game_post/create_game_post.js
+++ src/pages/create_game_post/create_game_post.js
@@ -17,18 +17,25 @@ import './create_game_post.css';
 
 
 export const CreateGamePost = () => {
+  const [newGameDate, setNewGameDate] = useState(0);
+  // 本来date型でデータを収集したいが、一旦ただの数値で格納
   const [newTeamName, setNewTeamName] = useState("");
   const [newCount, setNewCount] = useState(0);
   const [newPlace, setNewPlace] = useState("");
+  const [newStation, setNewStation] = useState("");
+  const [newCost, setNewCost] = useState(0);
+  const [newHelper, setNewHelper] = useState(0);
 
   const gamesCollectionRef = collection(db, "Games");
 
   const createGame = async () => {
-    await addDoc(gamesCollectionRef, { name: newTeamName, place: newPlace, count: Number(newCount) });
+    await addDoc(gamesCollectionRef, { date: newGameDate, name: newTeamName, place: newPlace, station: newStation, count: Number(newCount), cost: newCost, helper: newHelper });
     // フォーム送信後中身を空にする処理（まだうまくいってない）
+    setNewCount(0);
     setNewTeamName("");
     setNewCount(0);
     setNewPlace("");
+    setNewHelper(0);
 
   };
 
@@ -39,6 +46,14 @@ export const CreateGamePost = () => {
       <Card style={{ width: "40rem" , height:"15rem",borderRadius:"10px"}}>
         <Card.Body>
           <Card.Title className='createGamePostTitle'>募集ゲーム作成</Card.Title>
+          <input
+            className='form-control'
+            type="number"
+            placeholder="Date"
+            onChange={(event) => {
+              setNewGameDate(event.target.value);
+            }}
+          />
           <input
             className='form-control'
             placeholder="Team name..."
@@ -55,10 +70,32 @@ export const CreateGamePost = () => {
           />
           <input
             className='form-control'
+            placeholder="Station"
+            onChange={(event) => {
+              setNewStation(event.target.value);
+            }}
+          />
+          <input
+            className='form-control'
             type="number"
             placeholder="Count..."
             onChange={(event) => {
               setNewCount(event.target.value);
+            }}
+          />
+          <input
+            className='form-control'
+            type="number"
+            placeholder="Cost"
+            onChange={(event) => {
+              setNewCost(event.target.value);
+            }}
+          />
+          <input
+            className='form-control'
+            placeholder="Helper"
+            onChange={(event) => {
+              setNewHelper(event.target.value);
             }}
           />
         </Card.Body>
