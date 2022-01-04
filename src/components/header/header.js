d875e20ae360d8e596505f21d4e7322a12cc11d1
kyonishikawa <kyo24111@gmail.com>
2022-01-05 01:24:46 +0900

    login identification

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -13,8 +13,10 @@ import { useState } from 'react'
 // ヘッダーを表示するコンポーネント
 const Header = () => {
   const [username,setUsername] = useState("");
+  const [isLogin, setIsLogin] = useState(false);
   onAuthStateChanged(auth,async (user) => {
     if(user){
+      setIsLogin(true);
       const uid = user.uid;
       const email = user.email;
       const q = query(collection(db, "Users"),where("id","==",uid));
@@ -26,26 +28,71 @@ const Header = () => {
     }
   })
 
-  return(
-    <Navbar collapseOnSelect expand="lg" className="header_outline">
-      <Navbar.Brand href="/home" className="headerText">
-          Sandlot
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <div className="header_contents_outline">
-          <Nav className="mr-auto">
-            <Nav.Link href="/createteam" className="headerContents">Add team</Nav.Link>
-            <Nav.Link href="/myteam" className="headerContents">MyTeam</Nav.Link>
-            <Nav.Link href="/creategame" className="headerContents">Post game</Nav.Link>
-            <Nav.Link href="/teamtable" className="headerContents">Team table</Nav.Link>
-            <Nav.Link href="/login" className="headerContents">Log in</Nav.Link>
-            <Nav.Link href="/signup" className="headerContents">Sign up</Nav.Link>
-            {username}
-          </Nav>
-        </div>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+  if(isLogin){
+    return(
+      <Navbar collapseOnSelect expand="lg" className="header_outline">
+        <Navbar.Brand href="/home" className="headerText">
+            Sandlot
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <div className="header_contents_outline">
+            <Nav className="mr-auto">
+              <Nav.Link href="/createteam" className="headerContents">Add team</Nav.Link>
+              <Nav.Link href="/myteam" className="headerContents">MyTeam</Nav.Link>
+              <Nav.Link href="/creategame" className="headerContents">Post game</Nav.Link>
+              <Nav.Link href="/teamtable" className="headerContents">Team table</Nav.Link>
+              <Nav.Link href="/login" className="headerContents">Log in</Nav.Link>
+              <Nav.Link href="/signup" className="headerContents">Sign up</Nav.Link>
+              {username}
+            </Nav>
+          </div>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }else{
+    return(
+      <Navbar collapseOnSelect expand="lg" className="header_outline">
+        <Navbar.Brand href="/home" className="headerText">
+            Sandlot
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <div className="header_contents_outline">
+            <Nav className="mr-auto">
+              {/* <Nav.Link href="/createteam" className="headerContents">Add team</Nav.Link> */}
+              {/* <Nav.Link href="/myteam" className="headerContents">MyTeam</Nav.Link> */}
+              {/* <Nav.Link href="/creategame" className="headerContents">Post game</Nav.Link> */}
+              <Nav.Link href="/teamtable" className="headerContents">Team table</Nav.Link>
+              <Nav.Link href="/login" className="headerContents">Log in</Nav.Link>
+              <Nav.Link href="/signup" className="headerContents">Sign up</Nav.Link>
+              {username}
+            </Nav>
+          </div>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+  // return(
+  //   <Navbar collapseOnSelect expand="lg" className="header_outline">
+  //     <Navbar.Brand href="/home" className="headerText">
+  //         Sandlot
+  //     </Navbar.Brand>
+  //     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  //     <Navbar.Collapse id="responsive-navbar-nav">
+  //       <div className="header_contents_outline">
+  //         <Nav className="mr-auto">
+  //           <Nav.Link href="/createteam" className="headerContents">Add team</Nav.Link>
+  //           <Nav.Link href="/myteam" className="headerContents">MyTeam</Nav.Link>
+  //           <Nav.Link href="/creategame" className="headerContents">Post game</Nav.Link>
+  //           <Nav.Link href="/teamtable" className="headerContents">Team table</Nav.Link>
+  //           <Nav.Link href="/login" className="headerContents">Log in</Nav.Link>
+  //           <Nav.Link href="/signup" className="headerContents">Sign up</Nav.Link>
+  //           {username}
+  //         </Nav>
+  //       </div>
+  //     </Navbar.Collapse>
+  //   </Navbar>
+  // );
 }
 export default Header;
