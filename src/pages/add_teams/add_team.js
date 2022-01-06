b69b6a6c0853a16b427ba23486ca9276e0a7e175
kyonishikawa <kyo24111@gmail.com>
2022-01-07 08:59:55 +0900

    add_team update

--- src/pages/add_teams/add_team.js
+++ src/pages/add_teams/add_team.js
@@ -26,7 +26,6 @@ export const AddTeam = () => {
     if(user){
       const uid = user.uid;
       setUserId(uid);
-      
     }
   })
 
@@ -36,45 +35,59 @@ export const AddTeam = () => {
     navigate("/",{replace:true})
   };
   return(
-    <div>
-      <Card className='add_team_card'>
+    <div className='form_outline'>
+      <Card className='add_team_card_outline'>
         <Card.Body>
-          <Card.Title className='create_game_post_title'>Team Info</Card.Title>
-          <input
-            className='form-control'
-            placeholder="Team name..."
-            onChange={(event) => {
-              setNewTeamName(event.target.value);
-            }}
-          />
-          <input
-            className='form-control'
-            placeholder="Home Place..."
-            onChange={(event) => {
-              setNewPlace(event.target.value);
-            }}
-          />
-          <input
-            className='form-control'
-            type="number"
-            placeholder="Count..."
-            onChange={(event) => {
-              setNewCount(event.target.value);
-            }}
-          />
-          <input
-            className='form-control'
-            placeholder="Representative"
-            onChange={(event) => {
-              setNewCaptain(event.target.value);
-            }}
-          />
+          <Card.Title className='form_title'>
+            Team Info
+          </Card.Title>
+          <div className='form-group'>
+            <label>Team Name</label>
+            <input
+              className='form-control'
+              placeholder="Team name..."
+              onChange={(event) => {
+                setNewTeamName(event.target.value);
+              }}
+            />
+          </div>
+          <div className='form-group'>
+            <label>Home Ground</label>
+            <input
+              className='form-control'
+              placeholder="Home Ground..."
+              onChange={(event) => {
+                setNewPlace(event.target.value);
+              }}
+            />
+          </div>
+          <div className='form-group'>
+            <label>Email address</label>
+            <input
+              className='form-control'
+              type="number"
+              placeholder="Count..."
+              onChange={(event) => {
+                setNewCount(event.target.value);
+              }}
+            />
+          </div>
+          <div className='form-group'>
+            <label>Email address</label>
+            <input
+              className='form-control'
+              placeholder="Representative"
+              onChange={(event) => {
+                setNewCaptain(event.target.value);
+              }}
+            />
+          </div>
+          <form>
+            <Button className='form_button' onClick={() => createTeam()}>
+                Create team
+            </Button>
+          </form>
         </Card.Body>
-        <form>
-          <Button onClick={() => createTeam()}>
-              Create team
-          </Button>
-        </form>
       </Card>
 
 
