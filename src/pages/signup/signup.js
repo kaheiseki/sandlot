ecb60a0accf16a341b5e51fca7169efe335f31e2
kaheiseki <kaheiseki.code@gmail.com>
2022-01-06 22:17:44 +0900

    delete nouse param

--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react'
 import { useState } from "react";
-import {createUserWithEmailAndPassword,onAuthStateChanged} from "firebase/auth"
+import {createUserWithEmailAndPassword} from "firebase/auth"
 import {auth, db} from "../../firebase";
 import {
   collection,
@@ -9,7 +9,6 @@ import {
 import { Card , Button} from 'react-bootstrap';
 import './signup.css';
 import { useNavigate } from 'react-router-dom';
-import GameInfo from '../index/gameinfo';
 
 
 export const SignUp = () => {
@@ -18,7 +17,6 @@ export const SignUp = () => {
   const[email,setEmail] = useState("");
   const[password,setPassword] = useState("");
   const[confirmPassword,setConfirmPassword] = useState("");
-  const[isSignUp, setIsSignUp] = useState(false);
 
   const inputUsername = useCallback((e) => {
     setUsername(e.target.value)
@@ -62,48 +60,10 @@ export const SignUp = () => {
         })
       }
     }
-  }
-
-  // add_teamのhooks
-  const [newTeamName, setNewTeamName] = useState("");
-  const [newCount, setNewCount] = useState(0);
-  const [newPlace, setNewPlace] = useState("");
-  const [newCaptain, setNewCaptain] = useState("");
-  const [userId,setUserId] = useState("");
-
-  const inputTeamName = useCallback((e) => {
-    setNewTeamName(e.target.value)
-  },[setNewTeamName]);
-  const inputCaptain = useCallback((e) => {
-    setNewCaptain(e.target.value)
-  },[setNewCaptain]);
-  const inputPlace = useCallback((e) => {
-    setNewPlace(e.target.value)
-  },[setNewPlace]);
-  const inputCount = useCallback((e) => {
-    setNewCount(e.target.value)
-  },[setNewCount]);
-
-  const gamesCollectionRef = collection(db, "Teams");
-  onAuthStateChanged(auth,(user)=>{
-    if(user){
-      const uid = user.uid;
-      setUserId(uid);
-    }
-  })
-
-
-  const createTeam = async () => {
-    await addDoc(gamesCollectionRef, { name: newTeamName, place: newPlace, count: Number(newCount), captain: newCaptain, id: userId});
-    setNewTeamName("");
-    setNewCount("");
-    setNewPlace("");
-    setNewCaptain("");
-    console.log("add team succeed");
   };
-  if (isSignUp) {
-    return <GameInfo/>
-  }else{
+
+
+ 
     return(
       <div className='form_outline'>
         <Card className='form_groups'>
@@ -149,55 +109,6 @@ export const SignUp = () => {
           </div>
         </Card.Body>
       </Card>
-      {/* <Card className='add_team_card'>
-        <Card.Body>
-          <Card.Title className='create_game_post_title'>Team Register</Card.Title>
-          <div className='form-group'>
-            <label>Team name</label>
-            <input
-              className='form-control'
-              placeholder="Team name..."
-              value={newTeamName}
-              onChange={(event) => {
-                setNewTeamName(event.target.value);
-              }}
-            />
-          </div>
-          <div className='form-group'>
-            <label>Home Stadium</label>
-            <input
-              className='form-control'
-              placeholder="Home Place..."
-              value={newPlace}
-              onChange={(event) => {
-                setNewPlace(event.target.value);
-              }}
-            />
-          </div>
-          <div className='form-group'>
-            <label>Amount of Team Member</label>
-            <input
-              className='form-control'
-              type="number"
-              placeholder="Count..."
-              value={newCount}
-              onChange={(event) => {
-                setNewCount(event.target.value);
-              }}
-            />
-          </div>
-          <div className='form-group'>
-            <label>Representative</label>
-            <input
-              className='form-control'
-              placeholder="Representative"
-              value={newCaptain}
-              onChange={(event) => {
-                setNewCaptain(event.target.value);
-              }}
-            />
-          </div>
-        </Card.Body> */}
         <form>
           <Button
             onClick={Signup(username,email,password,confirmPassword)
@@ -208,6 +119,5 @@ export const SignUp = () => {
       {/* </Card> */}
     </div>
     )
-  }
 };
 
