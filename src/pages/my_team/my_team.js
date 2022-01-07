c59b4c836e3555f2c5b0ce21ab413ec14410d6e7
kyonishikawa <kyo24111@gmail.com>
2022-01-07 09:12:14 +0900

    myteam css pudate

--- src/pages/my_team/my_team.js
+++ src/pages/my_team/my_team.js
@@ -5,6 +5,8 @@ import { db } from '../../firebase'
 import { collection,getDocs,query,where,doc,updateDoc} from 'firebase/firestore'
 import { useState } from 'react'
 import {Card,Button} from 'react-bootstrap';
+import './my_team.css';
+
 
 export const MyTeam = () => {
 
@@ -29,54 +31,68 @@ export const MyTeam = () => {
 
   const teamsDocumentRef = doc(db, "Teams", uid);
   const upDate = async () => {
-    await updateDoc(teamsDocumentRef, { 
-      name: teamname, 
-      place: place, 
-      count: Number(count), 
+    await updateDoc(teamsDocumentRef, {
+      name: teamname,
+      place: place,
+      count: Number(count),
       captain: captain
     });
   };
 
   return (
-    <div>
-    <Card className='add_team_card'>
+    <div className='my_team_outline'>
+      <Card className='my_team_card_outline'>
         <Card.Body>
-          <Card.Title className='create_game_post_title'>My team</Card.Title>
-          <input
-            className='form-control'
-            defaultValue={teamname}
-            onChange={(event) => {
-              setTeamname(event.target.value);
-            }}
-          />
-          <input
-            className='form-control'
-            defaultValue = {place}
-            onChange={(e) => {
-              setPlace(e.target.value);
-            }}
-          />
-          <input
-            className='form-control'
-            type="number"
-            defaultValue = {count}
-            onChange={(event) => {
-              setCount(event.target.value);
-            }}
-          />
-          <input
-            className='form-control'
-            defaultValue={captain}
-            onChange={(event) => {
-              setCaptain(event.target.value);
-            }}
-          />
+          <Card.Title className='myteam_title'>
+            My team
+          </Card.Title>
+          <div className='form-group'>
+            <label>Team Name</label>
+            <input
+              className='form-control'
+              defaultValue={teamname}
+              onChange={(event) => {
+                setTeamname(event.target.value);
+              }}
+            />
+          </div>
+          <div className='form-group'>
+            <label>Home Ground</label>
+            <input
+              className='form-control'
+              defaultValue = {place}
+              onChange={(e) => {
+                setPlace(e.target.value);
+              }}
+            />
+          </div>
+          <div className='form-group'>
+            <label>Amount of Member</label>
+            <input
+              className='form-control'
+              type="number"
+              defaultValue = {count}
+              onChange={(event) => {
+                setCount(event.target.value);
+              }}
+            />
+          </div>
+          <div className='form-group'>
+            <label>Captain</label>
+            <input
+              className='form-control'
+              defaultValue={captain}
+              onChange={(event) => {
+                setCaptain(event.target.value);
+              }}
+            />
+          </div>
+          <form>
+            <Button className='myteam_button' onClick={() => upDate()}>
+                Update team
+            </Button>
+          </form>
         </Card.Body>
-        <form>
-          <Button onClick={() => upDate()}>
-              Update team
-          </Button>
-        </form>
       </Card>
     </div>
   )
