5381b02a76114743c201410da084922c30db8f62
kaheiseki <kaheiseki.code@gmail.com>
2022-01-11 15:08:36 +0900

    change routing in header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,6 +3,7 @@ import { Navbar, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './header.css';
 import { auth} from '../../firebase'
+import { BrowserRouter,Link } from "react-router-dom";
 
 
 
@@ -13,25 +14,45 @@ const Header = ({isLogin, username}) => {
   }
   if(isLogin){
     return(
-      <Navbar collapseOnSelect expand="lg" className="header_outline">
-        <Navbar.Brand href="/gamelist" className="headerText">
+      <BrowserRouter>
+        <Navbar collapseOnSelect expand="lg" className="header_outline">
+          <Navbar.Brand className="headerText">
+            <Link to = "/gamelist" >
             Sandlot
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <div className="header_contents_outline">
-            <Nav className="mr-auto">
-              <Nav.Link href="/gamelist" className="headerContents">Game List</Nav.Link>
-              <Nav.Link href="/creategame" className="headerContents">Post game</Nav.Link>
-              <Nav.Link href="/teamlist" className="headerContents">Team List</Nav.Link>
-              <Nav.Link href="/myteam" className="headerContents">MyTeam</Nav.Link>
-              <Nav.Link href="/" className="headerContents" onClick={()=>LogOut()}>Log out</Nav.Link>
-              <Nav.Link className="headerContents">{username}さん</Nav.Link>
-              {/* hoverした時クリッカブルに見える */}
-            </Nav>
-          </div>
-        </Navbar.Collapse>
-      </Navbar>
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <div className="header_contents_outline">
+              <Nav className="mr-auto">
+                <Nav.Link className="headerContents">
+                <Link to = "/gamelist">
+                  Game List
+                </Link>
+                </Nav.Link>
+                <Nav.Link  className="headerContents">
+                  <Link to = "/creategame">
+                    Post Game
+                  </Link>
+                </Nav.Link>
+                <Nav.Link className="headerContents">
+                  <Link to = "/teamlist">
+                    Team List
+                  </Link>
+                </Nav.Link>
+                <Nav.Link className="headerContents">
+                  <Link to = "/myteam">
+                    My Team
+                  </Link>
+                </Nav.Link>
+                <Nav.Link href="/" className="headerContents" onClick={()=>LogOut()}>Log out</Nav.Link>
+                <Nav.Link className="headerContents">{username}さん</Nav.Link>
+                {/* hoverした時クリッカブルに見える */}
+              </Nav>
+            </div>
+          </Navbar.Collapse>
+        </Navbar>
+      </BrowserRouter>
     );
   }else{
     return(
